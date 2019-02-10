c3d2b6cab297d6869d4355645b04bd38766b8563
Carrie Vrtis <cjvrtis@gmail.com>
2019-02-09 21:23:26 -0800

    Add eslint.

--- index.js
+++ index.js
@@ -3,34 +3,33 @@ const Eris = require('eris');
 const quaff = require('quaff');
 
 async function init() {
-
-    // Load config
-    const CONFIG = () => {
-        try {
-            return require('./config.json');
-        }
-        catch(e) {
-            console.error('CyBorg could not find a config.json file!');
-            process.exit(1);
-        }    
-    };
-
-    
-    // Load language packs
-    const LANG = await quaff('./lang');
-    let gLang = (LANG[CONFIG.lang] ? CONFIG.lang : 'en-US');
-
-    console.log(LANG[gLang].BOOT);
-
-    // Load secrets
-    const SECRET = await quaff('./secret');
-    if(!(SECRET.discord.TOKEN)) {
-        console.error(LANG[gLang].ERR_NOTOKEN);
-        process.exit(1);
+  // Load config
+  const CONFIG = async () => {
+    try {
+      return await JSON.parse(fs.readFile('./config.json'));
+    } catch (e) {
+      console.error('CyBorg could not find a config.json file!');
+      return false;
     }
-    let bot = new Eris(SECRET.discord.TOKEN);
-    console.log(bot);
+  };
+  if (!CONFIG) {
+    process.exit(1);
+  }
+
+  // Load language packs
+  const LANG = await quaff('./lang');
+  const gLang = (LANG[CONFIG.lang] ? CONFIG.lang : 'en-US');
+
+  console.log(LANG[gLang].BOOT);
+
+  // Load secrets
+  const SECRET = await quaff('./secret');
+  if (!(SECRET.discord.TOKEN)) {
+    console.error(LANG[gLang].ERR_NOTOKEN);
+    process.exit(1);
+  }
+  const bot = new Eris(SECRET.discord.TOKEN);
+  console.log(bot);
 }
 
 init();
-
